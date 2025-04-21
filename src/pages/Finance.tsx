b026ddc65f27bc30efe7ea8c178cd5011617f7b4
jamal <135423558+jamal-codez@users.noreply.github.com>
2025-04-21 01:14:21 +0100

    Update Finance.tsx

--- src/pages/Finance.tsx
+++ src/pages/Finance.tsx
@@ -308,7 +308,7 @@ export default function Finance() {
               </Card>
               <Card>
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">Average Transactinon Value</CardTitle>
+                  <CardTitle className="text-sm font-medium">Avg Transaction Value</CardTitle>
                   <CircleDollarSign className="h-4 w-4 text-muted-foreground" />
                 </CardHeader>
                 <CardContent>
