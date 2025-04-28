00f76f3b6ee32c76f8af7d8c68ef5a813cc1cd1b
jamal <135423558+jamal-codez@users.noreply.github.com>
2025-04-28 11:50:19 +0100

    jamal 2

--- src/components/SidebarNav.tsx
+++ src/components/SidebarNav.tsx
@@ -30,9 +30,9 @@ const navItems = [
   { title: "State Pricing", icon: Tag, path: "/pricing", allowedRoles: [0, 1] },
   { title: "Reports", icon: BarChart2, path: "/report", allowedRoles: [0, 1] },
   { title: "Delivery Process", icon: Truck, path: "/delivery-processing", allowedRoles: [0, 1, 2, 3] },
-  { title: "Pickup Process", icon: Truck, path: "/pickup-processing", allowedRoles: [0, 1, 2, 3] },
+  { title: "Release Orders", icon: Truck, path: "/pickup-processing", allowedRoles: [0, 1, 2, 3] },
   { title: "Offline Sales", icon: ClipboardList, path: "/offline-sales", allowedRoles: [0, 1, 2, 3] },
-  { title: "Order Verification", icon: FileText, path: "/order-verification", allowedRoles: [0, 1, 2] },
+  // { title: "Order Verification", icon: FileText, path: "/order-verification", allowedRoles: [0, 1, 2] },
   { title: "Payment Verification", icon: Banknote, path: "/payment-verify", allowedRoles: [0, 1, 2] },
   { title: "Staff Management", icon: User, path: "/users-management", allowedRoles: [0, 1] }
 ];
