3f3a1f5bad3a1195e0a213bc49efdcefe1775767
jamal <135423558+jamal-codez@users.noreply.github.com>
2025-05-03 06:13:29 +0100

    Update SidebarNav.tsx

--- src/components/SidebarNav.tsx
+++ src/components/SidebarNav.tsx
@@ -21,6 +21,11 @@ import {
 } from "lucide-react";
 import { Button } from './ui/button';
 
+// SUPERADMIN= 0,"SUPERADMIN"
+// ADMIN= 1,"General Admin"
+// FINANCE =2,"Finance Admin"
+// SALES=3,"Marketing Officer"
+// RELEASE=4,"Release Officer"
 
 const navItems = [
   { title: "Dashboard", icon: Home, path: "/dashboard", allowedRoles: [0, 1, 2, 3, 4] },
@@ -28,11 +33,11 @@ const navItems = [
   { title: "Inventory", icon: Fuel, path: "/inventory", allowedRoles: [0, 1, 2,4] },
   { title: "Customers", icon: Users, path: "/customers", allowedRoles: [0, 1, 2,3] },
   { title: "Finance", icon: Banknote, path: "/finance", allowedRoles: [0, 1, 2] },
-  { title: "Location Pricing", icon: Tag, path: "/pricing", allowedRoles: [0, 1, 2] },
+  { title: "Location Pricing", icon: Tag, path: "/pricing", allowedRoles: [0, 1, 2, 3] },
   { title: "Reports", icon: BarChart2, path: "/report", allowedRoles: [0, 1,2,3,4] },
   { title: "Delivery Process", icon: Truck, path: "/delivery-processing", allowedRoles: [0, 1, 2, 4] },
-  { title: "Release Orders", icon: Truck, path: "/pickup-processing", allowedRoles: [0, 1, 2, 3] },
-  { title: "Offline Sales", icon: ClipboardList, path: "/offline-sales", allowedRoles: [0, 1,4] },
+  { title: "Release Orders", icon: Truck, path: "/pickup-processing", allowedRoles: [0, 1,4] },
+  { title: "Offline Sales", icon: ClipboardList, path: "/offline-sales", allowedRoles: [0, 1,2,4] },
   // { title: "Order Verification", icon: FileText, path: "/order-verification", allowedRoles: [0, 1, 2] },
   { title: "Payment Verification", icon: Banknote, path: "/payment-verify", allowedRoles: [0, 1, 2] },
   { title: "Staff Management", icon: User, path: "/users-management", allowedRoles: [0, 1] }
