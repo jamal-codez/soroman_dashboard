b061001e24287d92224614c36acfa836a31c903d
muhdhabeeybx <muhdhabeeyb@gmail.com>
2026-01-01 22:14:19 +0100

    Roles

--- src/components/SidebarNav.tsx
+++ src/components/SidebarNav.tsx
@@ -36,7 +36,7 @@ const navItems = [
   { title: "Product Pricing", icon: Tag, path: "/pricing", allowedRoles: [0, 1, 3] },
   // { title: "Reports", icon: BarChart2, path: "/report", allowedRoles: [0, 1,2,3,4] },
   // { title: "Delivery Process", icon: Truck, path: "/delivery-processing", allowedRoles: [0, 1, 2, 4] },
-  { title: "Release/Ticketing", icon: Truck, path: "/pickup-processing", allowedRoles: [0, 1, 4] },
+  { title: "Release/Ticketing", icon: Truck, path: "/pickup-processing", allowedRoles: [0, 1, 3, 4] },
   // { title: "Offline Sales", icon: ClipboardList, path: "/offline-sales", allowedRoles: [0, 1,2,4] },
   // { title: "Order Verification", icon: FileText, path: "/order-verification", allowedRoles: [0, 1, 2] },
   { title: "Confirm Payment", icon: Banknote, path: "/payment-verify", allowedRoles: [0, 1, 2] },
